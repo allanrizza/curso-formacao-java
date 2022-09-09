2f7fce14f8e646fdfd2b3dfa8c40a6ef880f54b3
Allan G Rizza <allan.rizza@pumpkintech.com.br>
2022-09-08 23:11:30 -0300

    14.4 - Processamento concorrente entre duas threads

--- curso-formacao-java/src/cursojava/threads/AulaThread.java
+++ curso-formacao-java/src/cursojava/threads/AulaThread.java
@@ -4,13 +4,12 @@ import javax.swing.*;
 
 public class AulaThread {
     public static void main(String[] args) throws InterruptedException {
-
         new Thread() {
             public void run() {
                 //Código da rotina
                 // Código da rotina que quero executar em paralelo
-                for(int pos = 0; pos <= 10; pos++) {
-                    System.out.println("Teste sleep " + pos);
+                for(int pos = 1; pos <= 10; pos++) {
+                    System.out.println("Thread para envio de e-mail " + pos);
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -21,6 +20,21 @@ public class AulaThread {
 
         }.start(); /* Start liga a thread que fica processando paralelamente */
 
+        new Thread() {
+            public void run() {
+                //Código da rotina
+                // Código da rotina que quero executar em paralelo
+                for(int pos = 1; pos <= 10; pos++) {
+                    System.out.println("Thread para envio de notificação " + pos);
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+        }.start(); /* Start liga a thread que fica processando paralelamente */
 
 
         System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD");
