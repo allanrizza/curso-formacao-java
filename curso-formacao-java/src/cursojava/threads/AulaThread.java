c3bfcf9e7f93becb6a57f7d3556b10dbf020b2d3
Allan G Rizza <allan.rizza@pumpkintech.com.br>
2022-09-08 23:17:55 -0300

    14.5 - Thread em objeto e o Runnable

--- curso-formacao-java/src/cursojava/threads/AulaThread.java
+++ curso-formacao-java/src/cursojava/threads/AulaThread.java
@@ -4,40 +4,45 @@ import javax.swing.*;
 
 public class AulaThread {
     public static void main(String[] args) throws InterruptedException {
-        new Thread() {
-            public void run() {
-                //Código da rotina
-                // Código da rotina que quero executar em paralelo
-                for(int pos = 1; pos <= 10; pos++) {
-                    System.out.println("Thread para envio de e-mail " + pos);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-
-        }.start(); /* Start liga a thread que fica processando paralelamente */
-
-        new Thread() {
-            public void run() {
-                //Código da rotina
-                // Código da rotina que quero executar em paralelo
-                for(int pos = 1; pos <= 10; pos++) {
-                    System.out.println("Thread para envio de notificação " + pos);
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-
-        }.start(); /* Start liga a thread que fica processando paralelamente */
-
+        Thread threadEmail = new Thread(thread1);
+        threadEmail.start();
+        Thread threadNotificacao = new Thread(thread2);
+        threadNotificacao.start();
 
         System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD");
         JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
     }
+
+    private static Runnable thread1 = new Runnable() {
+        @Override
+        public void run() {
+            //Código da rotina
+            // Código da rotina que quero executar em paralelo
+            for(int pos = 1; pos <= 10; pos++) {
+                System.out.println("Thread para envio de e-mail " + pos);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    };
+
+    private static Runnable thread2 = new Runnable() {
+        @Override
+        public void run() {
+            //Código da rotina
+            // Código da rotina que quero executar em paralelo
+            for(int pos = 1; pos <= 10; pos++) {
+                System.out.println("Thread para envio de notificação " + pos);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    };
+
 }
