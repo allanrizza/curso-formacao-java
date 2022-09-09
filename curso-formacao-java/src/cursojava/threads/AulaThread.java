feec0771383b888ee002bd69fc09f2c424e06271
Allan G Rizza <allan.rizza@pumpkintech.com.br>
2022-09-08 23:19:28 -0300

    14.5 - Melhorando código

--- curso-formacao-java/src/cursojava/threads/AulaThread.java
+++ curso-formacao-java/src/cursojava/threads/AulaThread.java
@@ -3,7 +3,7 @@ package cursojava.threads;
 import javax.swing.*;
 
 public class AulaThread {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread threadEmail = new Thread(thread1);
         threadEmail.start();
         Thread threadNotificacao = new Thread(thread2);
@@ -13,34 +13,28 @@ public class AulaThread {
         JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
     }
 
-    private static Runnable thread1 = new Runnable() {
-        @Override
-        public void run() {
-            //Código da rotina
-            // Código da rotina que quero executar em paralelo
-            for(int pos = 1; pos <= 10; pos++) {
-                System.out.println("Thread para envio de e-mail " + pos);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+    private final static Runnable thread1 = () -> {
+        //Código da rotina
+        // Código da rotina que quero executar em paralelo
+        for(int pos = 1; pos <= 10; pos++) {
+            System.out.println("Thread para envio de e-mail " + pos);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     };
 
-    private static Runnable thread2 = new Runnable() {
-        @Override
-        public void run() {
-            //Código da rotina
-            // Código da rotina que quero executar em paralelo
-            for(int pos = 1; pos <= 10; pos++) {
-                System.out.println("Thread para envio de notificação " + pos);
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+    private final static Runnable thread2 = () -> {
+        //Código da rotina
+        // Código da rotina que quero executar em paralelo
+        for(int pos = 1; pos <= 10; pos++) {
+            System.out.println("Thread para envio de notificação " + pos);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     };
